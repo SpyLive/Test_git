6df293051e2ad3f0fe2d6b7d09026b77b7a7fbb1
SpyLive <spy_live@mail.ru>
2013-10-02 22:31:28 +0700

    23

--- Test_git/Class.cs
+++ Test_git/Class.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Test_git
+﻿namespace Test_git
 {
     public class Class
     {
-
+        
     }
 }
